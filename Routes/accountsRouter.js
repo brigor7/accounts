af67431e0164105bd39f6c61b6a10dc58321a7a9
=Bruno Igor <=brigor7@gmail.com>
2020-06-30 14:49:02 -0300

    Implementação do endpoint saque

--- Routes/accountsRouter.js
+++ Routes/accountsRouter.js
@@ -36,4 +36,36 @@ app.put('/deposito/:agencia/:conta/:value', async (req, res) => {
     res.status(500).send('Erro ao acessar Get(): ' + error);
   }
 });
+
+app.put('/saque/:agencia/:conta/:value', async (req, res) => {
+  try {
+    const account = await accountModel.findOne({
+      agencia: req.params.agencia,
+      conta: req.params.conta,
+    });
+
+    if (!account) {
+      res.status(404).send('Conta e Agencia não encontrados.');
+      return;
+    }
+    //Atualizando valor do saldo
+    if (account.balance - Number(req.params.value) < 0) {
+      res
+        .status(404)
+        .send('Conta com saldo negativo. Não é possivel fazer saque.');
+      return;
+    }
+
+    account.balance -= Number(req.params.value);
+    const newAccount = await accountModel.findOneAndUpdate(
+      { agencia: req.params.agencia, conta: req.params.conta },
+      account,
+      { new: true }
+    );
+    res.send(newAccount);
+  } catch (error) {
+    res.status(500).send('Erro ao acessar Get(): ' + error);
+  }
+});
+
 export { app as router };
